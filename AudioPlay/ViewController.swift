bd17169d6fb0b690071ad8c49967192fc6d819d8
Kentaro.Nishikata <kentaro.nishikata@gmail.com>
2021-02-11 18:36:55 +0900

    Initial Commit

---
+++ AudioPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioPlay
+//
+//  Created by 西方健太郎 on 2021/02/11.
+//  Copyright © 2021 idea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
